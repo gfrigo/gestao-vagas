8072eb6a9f285180edd4515cf57393c654feec55
Gabriel Frigo <gfrigo.sena@gmail.com>
2024-08-12 09:34:06 -0300

    security-config-adjust

--- src/main/java/br/com/gfrigo/gestao_vagas/security/SecurityConfig.java
+++ src/main/java/br/com/gfrigo/gestao_vagas/security/SecurityConfig.java
@@ -1,15 +1,20 @@
 package br.com.gfrigo.gestao_vagas.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration // Classe usada no start do spring para configuração
 public class SecurityConfig {
   
+  @Autowired
+  private SecurityFilter securityFilter;
+
   @Bean // Reescreve objetos gerenciados pelo Spring
   SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
     // csrf -> Requisição HTTP na tentativa de se passar por um usuário legítimo
@@ -19,10 +24,14 @@ public class SecurityConfig {
           .requestMatchers("/company/").permitAll()
           .requestMatchers("/auth/company").permitAll();
         auth.anyRequest().authenticated();
-      });
-    return http.build();
+      })
+      // Adiciona o filtro para receber a requisição e tratá-la antes da controller
+      .addFilterBefore(securityFilter, BasicAuthenticationFilter.class);
+    
+      return http.build();
   }
 
+  // Toda vez que for usado o AutoWired de PasswordEncoder será utilziado o BCryptPasswordEncoder para criptografar as senhas
   @Bean
   public PasswordEncoder passwordEncoder(){
     return new BCryptPasswordEncoder();
