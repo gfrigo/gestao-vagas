baef70f72b50da8423d146be98d172ebd2b0c095
Gabriel Frigo <gfrigo.sena@gmail.com>
2024-08-12 09:31:27 -0300

    add-encryption-candidate

--- src/main/java/br/com/gfrigo/gestao_vagas/modules/candidate/useCases/CreateCandidateUseCase.java
+++ src/main/java/br/com/gfrigo/gestao_vagas/modules/candidate/useCases/CreateCandidateUseCase.java
@@ -1,6 +1,7 @@
 package br.com.gfrigo.gestao_vagas.modules.candidate.useCases;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import br.com.gfrigo.gestao_vagas.exceptions.UserFoundException;
@@ -13,16 +14,20 @@ public class CreateCandidateUseCase {
   @Autowired // Autogerencia a instancia da classe CandidateRepository
   private CandidateRepository candidateRepository;
 
+  @Autowired
+  private PasswordEncoder passwordEncoder;
+
   public CandidateEntity execute(CandidateEntity candidateEntity){
     
-    this.candidateRepository
-    .findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail())
+  this.candidateRepository.findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail())
     .ifPresent((user) -> {
       throw new UserFoundException();
     });
     
+    var password = passwordEncoder.encode(candidateEntity.getPassword());
+    candidateEntity.setPassword(password);
+
     // Retorna CandidateEntity salvo no BD com todos os dados definidos em um JSON
     return this.candidateRepository.save(candidateEntity);
   }
-
 }
