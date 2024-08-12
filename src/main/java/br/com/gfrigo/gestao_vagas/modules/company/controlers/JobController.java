4b7c2d18ef013a7d42a80a467104b2aeb23f2c55
Gabriel Frigo <gfrigo.sena@gmail.com>
2024-08-12 09:32:02 -0300

    job-controller-refactor-token-jwt-company-id

--- src/main/java/br/com/gfrigo/gestao_vagas/modules/company/controlers/JobController.java
+++ src/main/java/br/com/gfrigo/gestao_vagas/modules/company/controlers/JobController.java
@@ -4,9 +4,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.gfrigo.gestao_vagas.modules.company.dto.CreateJobDTO;
 import br.com.gfrigo.gestao_vagas.modules.company.entities.JobEntity;
 import br.com.gfrigo.gestao_vagas.modules.company.useCases.CreateJobUseCase;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
+
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,7 +24,19 @@ public class JobController {
   private CreateJobUseCase createJobUseCase;
 
   @PostMapping("/")
-  public JobEntity create(@Valid @RequestBody JobEntity jobEntity){
+  public JobEntity create(@Valid @RequestBody CreateJobDTO createJobDTO, HttpServletRequest request){
+    // O subjectToken é um objeto, então, é necessário converter para UUID para jobEntity
+    var companyId = request.getAttribute("company_id");
+
+    // Evita que o usuário envie parâmetros adicionais na requisição, serão ignorados
+    // Evita que seja passado um company_id na requisição, também será ignorado
+    var jobEntity = JobEntity.builder()
+      .benefits(createJobDTO.getBenefits())
+      .companyId(UUID.fromString(companyId.toString()))
+      .description(createJobDTO.getDescription())
+      .level(createJobDTO.getLevel())
+      .build();
+
     return this.createJobUseCase.execute(jobEntity);
   }
 }
