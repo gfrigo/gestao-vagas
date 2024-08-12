7772ab092cf9cc368bfe5727de3f003a95a4c6d3
Gabriel Frigo <gfrigo.sena@gmail.com>
2024-08-12 09:33:41 -0300

    verify-token-jwt-and-confis

--- src/main/java/br/com/gfrigo/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
+++ src/main/java/br/com/gfrigo/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
@@ -1,5 +1,8 @@
 package br.com.gfrigo.gestao_vagas.modules.company.useCases;
 
+import java.time.Duration;
+import java.time.Instant;
+
 import javax.naming.AuthenticationException;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -43,6 +46,7 @@ public class AuthCompanyUseCase {
       // Se for igual -> Gerar Token JWT
       Algorithm algorithm = Algorithm.HMAC256(secretKey);
       var token = JWT.create().withIssuer("javagas")
+        .withExpiresAt(Instant.now().plus(Duration.ofHours(2)))
         .withSubject(company.getId().toString())
         .sign(algorithm);
 
